e0e277b535e77e5b5ea86c45d67161605bb91152
Victoroide <cvictorhugo39@gmail.com>
2024-07-19 18:19:29 -0400

    Fix eliminarRepetidos last number

--- src/Colas/ColaV.java
+++ src/Colas/ColaV.java
@@ -74,30 +74,26 @@ public class ColaV {
     public void eliminarRepetidos() {
         if (this.vacia()) return;
 
-        int i = Frente;
-        while (i != Atras) {
-            int j = (i + 1) % C.length;
-            int anterior = i;
-            while (j != (Atras + 1) % C.length) {
-                if (C[i] == C[j]) {
-                    while (j != Atras) {
-                        C[anterior] = C[j];
-                        anterior = j;
-                        j = (j + 1) % C.length;
+        int n = (Atras - Frente + C.length) % C.length + 1;
+        for (int i = 0; i < n; i++) {
+            int actual = C[(Frente + i) % C.length];
+            for (int j = i + 1; j < n; j++) {
+                if (C[(Frente + j) % C.length] == actual) {
+                    // Desplazar todos los elementos hacia la izquierda
+                    for (int k = j; k < n - 1; k++) {
+                        C[(Frente + k) % C.length] = C[(Frente + k + 1) % C.length];
                     }
                     Atras = (Atras - 1 + C.length) % C.length;
-                    if (Atras == Frente) {
-                        Frente = -1;
-                        Atras = -1;
-                        break;
-                    }
-                } else {
-                    anterior = j;
-                    j = (j + 1) % C.length;
+                    n--;  // Reducir el tamaño de la cola efectiva
+                    j--;  // Volver a comprobar la posición j
                 }
             }
-            if (Frente == -1) break;
-            i = (i + 1) % C.length;
+        }
+
+        // Ajustar Frente y Atras si la cola está vacía
+        if (Frente == (Atras + 1) % C.length) {
+            Frente = -1;
+            Atras = -1;
         }
     }
 
